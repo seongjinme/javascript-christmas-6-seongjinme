73a7fb5ba9bda319742b81f5e99cc3188335959b
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-13 22:48:59 +0900

    refactor: 에러 메시지의 "[ERROR]" 접두사 문구 상수화

--- src/constant/ErrorMessage.js
+++ src/constant/ErrorMessage.js
@@ -1,4 +1,5 @@
 const ERROR_MESSAGE = Object.freeze({
+  prefix: '[ERROR]',
   invalidDateNumber: '유효하지 않은 날짜입니다. 다시 입력해 주세요.',
   invalidOrder: '유효하지 않은 주문입니다. 다시 입력해 주세요.',
 });
--- src/error/CustomError.js
+++ src/error/CustomError.js
@@ -1,5 +1,7 @@
+import ERROR_MESSAGE from '../constant/ErrorMessage.js';
+
 class CustomError extends Error {
-  static PREFIX = '[ERROR]';
+  static PREFIX = ERROR_MESSAGE.prefix;
 
   constructor(errorMessage) {
     super(`\n${CustomError.PREFIX} ${errorMessage}\n`);
