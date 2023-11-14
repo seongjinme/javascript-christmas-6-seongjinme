a28191f7d13b131b29bda9c1b9888fd25d57ff74
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-14 22:54:31 +0900

    refactor: 메뉴 데이터 구조 개선

--- src/constant/MenuItem.js
+++ src/constant/MenuItem.js
@@ -1,66 +1,57 @@
+import deepFreeze from '../util/DeepFreeze.js';
 import MENU_CATEGORY from './MenuCategory.js';
 
-const MENU_ITEM = Object.freeze({
-  mushroomSoup: {
-    name: '양송이수프',
+const MENU_ITEM = {
+  양송이수프: {
     price: 6_000,
     category: `${MENU_CATEGORY.appitizer}`,
   },
-  tapas: {
-    name: '타파스',
+  타파스: {
     price: 5_500,
     category: `${MENU_CATEGORY.appitizer}`,
   },
-  caesarSalad: {
-    name: '시저샐러드',
+  시저샐러드: {
     price: 8_000,
     category: `${MENU_CATEGORY.appitizer}`,
   },
-  tBoneSteak: {
-    name: '티본스테이크',
+  티본스테이크: {
     price: 55_000,
     category: `${MENU_CATEGORY.main}`,
   },
-  barbecueRibs: {
-    name: '바비큐립',
+  바비큐립: {
     price: 54_000,
     category: `${MENU_CATEGORY.main}`,
   },
-  seafoodPasta: {
-    name: '해산물파스타',
+  해산물파스타: {
     price: 35_000,
     category: `${MENU_CATEGORY.main}`,
   },
-  christmasPasta: {
-    name: '해산물파스타',
+  크리스마스파스타: {
     price: 25_000,
     category: `${MENU_CATEGORY.main}`,
   },
-  chocoCake: {
-    name: '초코케이크',
+  초코케이크: {
     price: 15_000,
     category: `${MENU_CATEGORY.dessert}`,
   },
-  icecream: {
-    name: '아이스크림',
+  아이스크림: {
     price: 5_000,
     category: `${MENU_CATEGORY.dessert}`,
   },
-  cokeZero: {
-    name: '제로콜라',
+  제로콜라: {
     price: 3_000,
     category: `${MENU_CATEGORY.beverage}`,
   },
-  redWine: {
-    name: '레드와인',
+  레드와인: {
     price: 60_000,
     category: `${MENU_CATEGORY.beverage}`,
   },
-  champagne: {
-    name: '샴페인',
+  샴페인: {
     price: 25_000,
     category: `${MENU_CATEGORY.beverage}`,
   },
-});
+};
+
+deepFreeze(MENU_ITEM);
 
 export default MENU_ITEM;
