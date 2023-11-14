7a421f9bad033bfeabcf8b20e1d09d2e04b34907
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-14 23:52:19 +0900

    test: 예상 방문 날짜 입력값 테스트 조건 추가

--- __tests__/DateNumberValidatorTest.js
+++ __tests__/DateNumberValidatorTest.js
@@ -7,19 +7,22 @@ describe('예상 방문 날짜 입력값 검증 테스트', () => {
       dateNumberValidator('1a');
     }).toThrow(ERROR_MESSAGE.prefix);
   });
+
   test('입력값 앞에 0이 포함된 경우 예외가 발생한다.', () => {
     expect(() => {
       dateNumberValidator('03');
     }).toThrow(ERROR_MESSAGE.prefix);
   });
+
   test('입력값에 소수점이 포함된 경우 예외가 발생한다.', () => {
     expect(() => {
       dateNumberValidator('2.8');
     }).toThrow(ERROR_MESSAGE.prefix);
   });
-  test('입력값이 1-31 사이의 숫자가 아닌 경우 예외가 발생한다.', () => {
+
+  test.each([['0'], ['32']])('입력값이 1-31 사이의 숫자가 아닌 경우 예외가 발생한다.', (input) => {
     expect(() => {
-      dateNumberValidator('32');
+      dateNumberValidator(input);
     }).toThrow(ERROR_MESSAGE.prefix);
   });
 });
