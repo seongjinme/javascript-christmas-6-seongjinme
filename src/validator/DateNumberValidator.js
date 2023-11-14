075c2cf2676854cd6820a8255de77c5689a8b870
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-14 22:56:29 +0900

    refactor: 예상 방문 날짜 검증 모듈명 및 유형 변경
    - InputValidator 클래스 => dateNumberValidator 함수로 변경
    - 검증 로직은 이전과 동일하게 유지

---
+++ src/validator/DateNumberValidator.js
@@ -0,0 +1,20 @@
+import ERROR_MESSAGE from '../constant/ErrorMessage.js';
+import SETTING from '../constant/Setting.js';
+import CustomError from '../error/CustomError.js';
+
+function dateNumberValidator(dateNumber) {
+  if (isNaN(dateNumber)) {
+    throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
+  }
+  if (dateNumber.toString().charAt(0) === '0') {
+    throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
+  }
+  if (!Number.isInteger(Number(dateNumber))) {
+    throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
+  }
+  if (parseInt(dateNumber) < SETTING.eventMinDateNumber || parseInt(dateNumber) > SETTING.eventMaxDateNumber) {
+    throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
+  }
+}
+
+export default dateNumberValidator;
