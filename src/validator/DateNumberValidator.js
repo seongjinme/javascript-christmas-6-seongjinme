420ac03300f34666edb0b370fa231a2507e8e46f
Seongjin Hong <hongseongjin.to@gmail.com>
2023-11-15 21:23:43 +0900

    style: 각 검증조건별 줄 바꿈 추가

--- src/validator/DateNumberValidator.js
+++ src/validator/DateNumberValidator.js
@@ -6,12 +6,15 @@ function dateNumberValidator(dateNumber) {
   if (isNaN(dateNumber)) {
     throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
   }
+
   if (dateNumber.toString().charAt(0) === '0') {
     throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
   }
+
   if (!Number.isInteger(Number(dateNumber))) {
     throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
   }
+
   if (parseInt(dateNumber) < SETTING.eventMinDateNumber || parseInt(dateNumber) > SETTING.eventMaxDateNumber) {
     throw new CustomError(ERROR_MESSAGE.invalidDateNumber);
   }
